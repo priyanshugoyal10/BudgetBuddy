f098a721c9d80649bdd47edfb23a755392c380d3
Priyanshu Goyal <goyalpriyanshu814@gmail.com>
2023-07-05 23:30:06 +0530

    react setup done

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
