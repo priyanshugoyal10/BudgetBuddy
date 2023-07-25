48c355b2c020c41e923c63722ee1779628c55f69
Priyanshu Goyal <goyalpriyanshu814@gmail.com>
2023-07-25 23:25:10 +0530

    transaction ui error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
-
 import Register from "./pages/Register";
 
 function App() {
